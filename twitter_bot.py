ce9670b7a8c63950af51d2b8921fbe4353ba6a3a
Eli Williams <eli.t.williams@gmail.com>
2020-10-02 14:22:02 -0400

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,34 +1,53 @@
 from twitter import Twitter, OAuth, TwitterHTTPError
 import time
+from typing import List
 
-OAUTH_TOKEN = 'xxxxx'
-OAUTH_SECRET = 'xxxxx'
-CONSUMER_KEY = 'xxxxx'
-CONSUMER_SECRET = 'xxxxx'
+OAUTH_TOKEN = "xxxxx"
+OAUTH_SECRET = "xxxxx"
+CONSUMER_KEY = "xxxxx"
+CONSUMER_SECRET = "xxxxx"
 
-t = Twitter(auth=OAuth(OAUTH_TOKEN,OAUTH_SECRET,CONSUMER_KEY,CONSUMER_SECRET))
+t = Twitter(auth=OAuth(OAUTH_TOKEN, OAUTH_SECRET, CONSUMER_KEY, CONSUMER_SECRET))
 
-def search_tweets(q, count=100):
-    return t.search.tweets(q=q, result_type='recent', count=count)
 
-# Try to favorite a Tweet, throw error if already favorited
+def search_tweets(q: str, count: int =100):
+    """
+    Search all tweets containing query phrase "q".
+    """
+    return t.search.tweets(q=q, result_type="recent", count=count)
 
-def fav_tweet(tweet):
+
+def fav_tweet(tweet: Twitter):
+    """
+    Favorite a given tweet.
+    """
     try:
-        result = t.favorites.create(_id=tweet['id'])
-        print "Favorited: %s" % (result['text'])
+        result = t.favorites.create(_id=tweet["id"])
+        print "Favorited: %s" % (result["text"])
         return result
     except TwitterHTTPError as e:
         print "Error: ", e
         return None
 
-def auto_fav(q, count=100):
+
+def auto_fav(q: str, count: int = 100):
+    """
+    Repeatedly favorite "count" number number of tweets containing query phrase "q".
+    """
     result = search_tweets(q, count)
-    a = result['statuses'][0]['user']['screen_name']
-    print a
+    a = result["statuses"][0]["user"]["screen_name"]
+    print (a)
     success = 0
-    for tweet in result['statuses']:
+    for tweet in result["statuses"]:
         if fav_tweet(tweet) is not None:
             success += 1
-    print "We Favorited a total of %i out of %i tweets" % (success,
-          len(result['statuses']))
+    print ("We Favorited a total of %i out of %i tweets" % (success, len(result["statuses"])))
+
+
+def main():
+    auto_fav(q="ice cream", count=250)
+    return None
+
+
+if __name__ == "__main__":
+    main()
